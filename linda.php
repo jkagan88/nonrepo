dec18d9ecd2dc9504c4b2eb7ffec7f1890620d32
jkagan88 <kahanjoseph@gmail.com>
2013-08-15 18:43:28 -0400

    a program from linda

---
+++ linda.php
@@ -0,0 +1,31 @@
+<?php
+
+  class Address{
+  //class can have any number of properties
+    public $streetAddress_1;
+    public $townName;
+    public $stateName;
+    public $postalCode;
+    public $countryName;
+  
+  //Method to display address in HTMLi
+    function display() {
+      $output = '';
+      $output .= $this->streetAddress_1;
+      if ($this .= (streetAddress_2){
+        $output .= '<br>' . $this->streetaddress_2};
+      $output .='<br>';
+      $output .=$this->townName . ',' . $this->statename;
+      $output .= '' . $this->postalCode;
+      $output .= '<br>';
+      $output .= $this-> countryName;
+      
+      return $output;
+    }
+  }  
+      $address = new Address;
+      $address ->streetAdress_1 ='555 Fake Street';
+      $address ->townName = 'Townsville';
+      $address ->stateName = 'Hicks';
+      $address ->countryName = 'USA'
+      $address ->postalCode = '11111';
