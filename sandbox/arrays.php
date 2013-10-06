51ac0d8c88d0d8c548bacc58cb059500bf704711
jkagan88 <kahanjoseph@gmail.com>
2013-10-06 15:41:33 -0400

    random

--- sandbox/arrays.php
+++ sandbox/arrays.php
@@ -3,5 +3,10 @@
    echo $numbers[2];
    echo "<br />";
    $mixed = array(6,"dog",7,array("x","y","z"));
-   echo print_r($mixed);
+   
+   echo print_r($mixed) ;
+   echo "<br />";
+   echo $mixed[3];  
+   echo $mixed[3][1];
+
 ?>
